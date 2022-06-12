217ec7dae2b4decb23a522c9e8e563529e9afb3d
Unknown807 <milvovangveric@gmail.com>
2022-06-12 14:01:04 +0100

    Added functionality to search by keywords or title to GalleryItemData

--- Local-Gallery/GalleryItemData.cs
+++ Local-Gallery/GalleryItemData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Utilities
 {
@@ -20,6 +21,48 @@ namespace Utilities
         {
             string jsonString = JsonSerializer.Serialize(data);
             File.WriteAllText("../../../GallerySave.json", jsonString);
+        }
+
+        public static List<GalleryItemData>? searchGallery(string text)
+        {
+            List<GalleryItemData> newItems = new List<GalleryItemData>();
+
+            // Search by keywords
+            if (text.Contains("#"))
+            {
+                searchByKeyWords(ref newItems, text);
+            } else // Search by title
+            {
+                searchByTitle(ref newItems, text);
+            }
+
+            return newItems;
+        }
+
+        private static void searchByKeyWords(ref List<GalleryItemData>? newItems, string text)
+        {
+            List<GalleryItemData>? currentItems = getCurrentGallery();
+            MatchCollection matches = Regex.Matches(text, @"#[A-Za-z0-9]+");
+            foreach (GalleryItemData item in currentItems)
+            {
+                bool flag = true;
+                foreach (Match match in matches)
+                {
+                    flag &= item.Desc.Contains(match.Value);
+                    if (!flag) break;
+                }
+
+                if (flag) newItems.Add(item);
+            }
+        }
+
+        private static void searchByTitle(ref List<GalleryItemData>? newItems, string text)
+        {
+            List<GalleryItemData>? currentItems = getCurrentGallery();
+            foreach (GalleryItemData item in currentItems)
+            {
+                if (item.Title.ToLower().Contains(text.ToLower())) newItems.Add(item);
+            }
         }
 
     }
